2f556efefd634c7dfa237c704e083b8f8dd9ccce
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-02-21 18:52:22 +0100

    Update discrete_partial_reference.py

--- discrete_partial_reference.py
+++ discrete_partial_reference.py
@@ -13,6 +13,68 @@ import jdcoot
 from jdcoot.comp import comp_
 from jdcoot.coot import cot_numpy
 from jdcoot.utils import *
+from itertools import chain
+
+def discrete_labels(source, prop_source, target, prop_target):
+
+    n_source = len(source.Z)
+    n_target = len(target.Z)
+    source_levels = np.unique(source.Z)
+    target_levels = np.unique(target.Z)
+
+    if len(source_levels) > 2:
+    
+        del_idx = [k for k in source_levels if sum(source.Z == k) < 0.01 * len(source.Z)]
+
+        source = source.loc[~np.in1d(source.Z, del_idx), :].reset_index(drop=True)
+    
+    if len(target_levels) > 2:
+
+        del_idx = [k for k in target_levels if sum(target.Z == k) < 0.01 * len(target.Z)]
+
+        target = target.loc[~np.in1d(target.Z, del_idx), :].reset_index(drop=True)
+    
+    # number of observations kept referenced by the min number of available observation per class
+    source_freq = min(np.unique(source.Z, return_counts=True)[1])
+    target_freq = min(np.unique(target.Z, return_counts=True)[1])
+    
+    kept_source = []
+    for k in source_levels:
+        kept_source.append(np.random.choice(np.where(source.Z == k)[0], source_freq, replace=False))
+    
+    source = source.loc[chain(*kept_source), :].reset_index(drop=True)
+    
+    kept_target = []
+    for k in target_levels:
+        kept_target.append(np.random.choice(np.where(target.Z == k)[0], target_freq, replace=False))
+
+    target = target.loc[chain(*kept_target), :].reset_index(drop=True)
+    
+    source_labels = np.array([]).astype(int)
+
+    for k in source_levels:
+        a = np.random.choice(np.where(source.Z == k)[0], 
+                             math.ceil(prop_source * sum(source.Z == k)), 
+                             replace=False)
+
+        source_labels = np.append(source_labels, a)
+    
+    target_labels = np.array([]).astype(int)
+    for k in target_levels:
+        b = np.random.choice(np.where(target.Z == k)[0], 
+                             math.ceil(prop_target * sum(target.Z == k)), 
+                             replace=False)
+
+        target_labels = np.append(target_labels, b)
+    
+
+    lsource = np.full(n_source, True)
+    ltarget = np.full(n_target, True)
+
+    lsource[source_labels] = False
+    ltarget[target_labels] = False
+
+    return lsource, ltarget
 
 
 def discrete_partial_reference( source, target, source_test, target_test) :
@@ -26,10 +88,10 @@ def discrete_partial_reference( source, target, source_test, target_test) :
     source_labels, target_labels = discrete_labels(source, prop_source, target, prop_target)
 
     source_train = source.drop(columns = 'Y')
-    source_train.loc[np.setdiff1d(np.arange(0, np.shape(source)[0]), source_labels), 'Z'] = -1
+    source_train.loc[source_labels, 'Z'] = -1
     
     target_train = target.drop(columns = 'Y')
-    target_train.loc[np.setdiff1d(np.arange(0, np.shape(target)[0]), target_labels), 'Z'] = -1
+    target_train.loc[target_labels, 'Z'] = -1
     
     def clf_seq(shape, nClass):
         model = tf_keras.Sequential([
@@ -76,9 +138,8 @@ def discrete_partial_reference( source, target, source_test, target_test) :
     z_test2 = clf2.predict(source_train.loc[source_train.Z == -1, source_train.columns != 'Z'])
     z_test2 = enc.inverse_transform(z_test2).reshape(-1)
     
-    perf_ref2 = (sum(z_test == target.loc[np.setdiff1d(np.arange(0, np.shape(target)[0]), target_labels), 'Z']) + sum(
-            z_test2 == source.loc[np.setdiff1d(np.arange(0, np.shape(source)[0]), source_labels), 'Z'])) / (
-                                              len(z_test) + len(z_test2))
+    perf_ref2 = (  sum(z_test == target.loc[target_labels, 'Z']) 
+                 + sum(z_test2 == source.loc[source_labels, 'Z'])) / (len(z_test) + len(z_test2))
 
     return perf_ref, perf_ref2
 
