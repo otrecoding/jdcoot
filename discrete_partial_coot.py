8624d681033ccee766f9431e44f7d60780ec274e
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2025-02-26 21:39:21 +0100

    Update discrete_partial_coot.py

--- discrete_partial_coot.py
+++ discrete_partial_coot.py
@@ -13,6 +13,8 @@ import jdcoot
 from jdcoot.comp import comp_
 from jdcoot.coot import cot_numpy
 from itertools import chain
+from sklearn.model_selection import train_test_split
+from jdcoot.utils import xcolumns
 
 
 def discrete_partial_coot(source, target, test_source, test_target):
@@ -20,14 +22,34 @@ def discrete_partial_coot(source, target, test_source, test_target):
     prop_source = 0.1
     prop_target = 0.1
 
-    source_train, source_test = train_test_split(source, test_size = prop_source, stratify = source.Z)
-    target_train, target_test = train_test_split(target, test_size = prop_target, stratify = target.Z)
+    source_levels = np.unique(source.Z)
+    target_levels = np.unique(target.Z)
+
+    nClass = len(np.union1d(source_levels, target_levels))
+    categories=[np.arange(nClass)]
+
+    encoder = onehot(handle_unknown='ignore', sparse_output=False, categories=categories)
+
+    def one_hot(z):
+        return encoder.fit_transform(z.reshape(-1, 1))
+
+    def one_cold(z):
+        return encoder.inverse_transform(z).reshape(-1)
+
+    x_source = source.loc[:, xcolumns(source)].values
+    x_target = target.loc[:, xcolumns(target)].values
+
+    z_source = source.Z.values
+    z_target = target.Z.values
+
+    source_train, source_test = train_test_split(source, test_size = prop_source, stratify = z_source)
+    target_train, target_test = train_test_split(target, test_size = prop_target, stratify = z_target)
 
     x_source_train = source_train.loc[:, xcolumns(source)].values
-    z_source_train = enc.fit_transform(source_train.Z.values.reshape(-1, 1))
+    z_source_train = one_hot(source_train.Z.values)
 
     x_target_train = target_train.loc[:, xcolumns(target)].values
-    z_target_train = enc.fit_transform(target_train.Z.values.reshape(-1, 1))
+    z_target_train = one_hot(target_train.Z.values)
 
     x_source_test = source_test.loc[:, xcolumns(source)].values
     z_source_test = source_test.Z.values
@@ -36,91 +58,77 @@ def discrete_partial_coot(source, target, test_source, test_target):
     z_target_test = target_test.Z.values
 
     def compute_cost_matrix(ys, yt, v=10000):
-        M = ot.dist(ys.values.reshape(-1, 1), yt.values.reshape(-1, 1), metric=comp_(v))
+        M = ot.dist(ys.reshape(-1, 1), yt.reshape(-1, 1), metric=comp_(v))
         return M
     
-    # Source labelled data learning
-    Source_indexes_labelled = np.where(Z_training_data_source['Z'] != -1)[0]
-    
-    M_lin = compute_cost_matrix(yt=Z_training_data_Target['Z'],
-                                ys=Z_training_data_source.loc[Source_indexes_labelled, 'Z'])
-    Ts, Tv, cost = cot_numpy(
-        X1=Z_training_data_source.loc[Source_indexes_labelled, Z_training_data_source.columns != 'Z'],
-        X2=Z_training_data_Target.loc[:, Z_training_data_Target.columns != 'Z'],
-        niter=100, C_lin=M_lin,
-        algo='sinkhorn', reg=1,
-        algo2='emd', verbose=False)
-    # Target estimation
-    enc = onehot(handle_unknown='ignore', sparse_output=False,
-                 categories=[np.arange(len(np.union1d(np.unique(S['Z']), np.unique(T['Z']))))])
-    zs_onehot = enc.fit_transform(S.loc[Source_indexes_labelled, 'Z'].values.reshape(-1, 1))
-    zt_onehot_estimated = len(T.loc[:, 'Z']) * np.dot(Ts.T, zs_onehot)
-    zt_estimated = enc.inverse_transform(zt_onehot_estimated).reshape(-1)
-    
-    # Target labelled data learning
-    Target_indexes_labelled = np.where(Z_training_data_Target['Z'] != -1)[0]
-    M_lin = compute_cost_matrix(yt=Z_training_data_source['Z'],
-                                ys=Z_training_data_Target.loc[Target_indexes_labelled, 'Z'])
-    Ts, Tv, cost = cot_numpy(
-        X1=Z_training_data_Target.loc[Target_indexes_labelled, Z_training_data_Target.columns != 'Z'],
-        X2=Z_training_data_source.loc[:, Z_training_data_source.columns != 'Z'],
-        niter=100, C_lin=M_lin,
-        algo='sinkhorn', reg=1,
-        algo2='emd', verbose=False)
-    # Source estimation
-    enc = onehot(handle_unknown='ignore', sparse_output=False,
-                 categories=[np.arange(len(np.union1d(np.unique(S['Z']), np.unique(T['Z']))))])
-    zt_onehot = enc.fit_transform(T.loc[Target_indexes_labelled, 'Z'].values.reshape(-1, 1))
-    zs_onehot_estimated = len(S.loc[:, 'Z']) * np.dot(Ts.T, zt_onehot)
-    zs_estimated = enc.inverse_transform(zs_onehot_estimated).reshape(-1)
-    
-    if len(np.setdiff1d(np.arange(0, np.shape(T)[0]), z_labelled_target)) != 0 and len(
-            np.setdiff1d(np.arange(0, np.shape(S)[0]), z_labelled_source)) != 0:
-        perf_coot = (sum(
-            T.loc[np.setdiff1d(np.arange(0, np.shape(T)[0]), z_labelled_target), 'Z'] == zt_estimated[
-                np.setdiff1d(np.arange(0, np.shape(T)[0]), z_labelled_target)]) + sum(
-            S.loc[np.setdiff1d(np.arange(0, np.shape(S)[0]), z_labelled_source), 'Z'] == zs_estimated[
-                np.setdiff1d(np.arange(0, np.shape(S)[0]), z_labelled_source)])) / (
-                                          len(np.setdiff1d(np.arange(0, np.shape(T)[0]),
-                                                           z_labelled_target)) + len(
-                                      np.setdiff1d(np.arange(0, np.shape(S)[0]), z_labelled_source)))
-    else:
-        perf_coot = (sum(T.loc[:, 'Z'] == zt_estimated) + sum(S.loc[:, 'Z'] == zs_estimated)) / (len(zt_estimated) + len(zs_estimated))
-    
-    #############train classifier and evaluate the performance on test
-    def clf_seq(shape, nClass):
-        model = tf_keras.Sequential([
-            Dense(units=128, input_shape=shape, activation='sigmoid'),
-            Dense(units=nClass, activation='sigmoid')])
-        return model
-    
-    vfunc = np.vectorize(lambda arr: 'X' in arr)
-    fe_sizeT = len(xcolumns(T))  # Nombre de variables de Target
-    shapeT = (fe_sizeT,)
-    loss = 'categorical_crossentropy'
-    clfT = clf_seq(shapeT, nClass=len(np.union1d(np.unique(S.Z), np.unique(T.Z))))
-    clfT.compile(optimizer='Adam', loss=loss, metrics=['accuracy'])
-    
-    fe_sizeS = len(xcolumns(S))  # Nombre de variables de Target
-    shapeS = (fe_sizeS,)
-    loss = 'categorical_crossentropy'
-    clfS = clf_seq(shapeS, nClass=len(np.union1d(np.unique(S['Z']), np.unique(T['Z']))))
-    clfS.compile(optimizer='Adam', loss=loss, metrics=['accuracy'])
-    
-    clfT.fit(T.loc[:, xcolumns(T)], enc.fit_transform(zt_estimated.reshape(-1, 1)), batch_size=10,
-             epochs=20, verbose=0)  # we train the classifier with target data estimated
-    clfS.fit(S.loc[:, xcolumns(S)], enc.fit_transform(zs_estimated.reshape(-1, 1)), batch_size=10,
-             epochs=20, verbose=0)  # we train the classifier with target data estimated
-    
-    zt_test = clfT.predict(T_test.loc[:, xcolumns(T_test)])
-    zs_test = clfS.predict(S_test.loc[:, xcolumns(S_test)])
-    
-    zt_test = enc.inverse_transform(zt_test).reshape(-1)
-    zs_test = enc.inverse_transform(zs_test).reshape(-1)
-    
-    perf_coot_test = (sum(zt_test == T_test.Z) + sum(zs_test == S_test.Z)) / (len(zs_test) + len(zt_test))
+    M_lin = compute_cost_matrix(yt=z_target, ys=z_source)
 
-    return perf_coot, perf_coot_test
+    Ts, Tv, cost = cot_numpy( X1=x_source, X2=x_target,
+         niter=100, C_lin=M_lin,
+         algo='sinkhorn', reg=1,
+         algo2='emd', verbose=False)
+
+    # Target estimation
+
+    zt_estimated = one_cold(len(z_target) * np.dot(Ts.T, z_source))
+
+    M_lin = compute_cost_matrix(yt=z_source_train, ys=z_target_train)
+
+    Ts, Tv, cost = cot_numpy( X1=x_target_train, X2=x_source_train,
+         niter=100, C_lin=M_lin,
+         algo='sinkhorn', reg=1,
+         algo2='emd', verbose=False)
+
+    # Source estimation
+    zt_onehot = one_hot(z_target_train)
+    zs_onehot_estimated = len(z_source) * np.dot(Ts.T, zt_onehot)
+    zs_estimated = one_cold(zs_onehot_estimated)
+     
+    perf_coot = (sum(z_target_test == zt_estimated))
+    #             np.setdiff1d(np.arange(0, np.shape(T)[0]), z_labelled_target)]) + sum(
+    #         S.loc[np.setdiff1d(np.arange(0, np.shape(S)[0]), z_labelled_source), 'Z'] == zs_estimated[
+    #             np.setdiff1d(np.arange(0, np.shape(S)[0]), z_labelled_source)])) / (
+    #                                       len(np.setdiff1d(np.arange(0, np.shape(T)[0]),
+    #                                                        z_labelled_target)) + len(
+    #                                   np.setdiff1d(np.arange(0, np.shape(S)[0]), z_labelled_source)))
+    # else:
+    #     perf_coot = (sum(T.loc[:, 'Z'] == zt_estimated) + sum(S.loc[:, 'Z'] == zs_estimated)) / (len(zt_estimated) + len(zs_estimated))
+    # 
+    # #############train classifier and evaluate the performance on test
+    # def clf_seq(shape, nClass):
+    #     model = tf_keras.Sequential([
+    #         Dense(units=128, input_shape=shape, activation='sigmoid'),
+    #         Dense(units=nClass, activation='sigmoid')])
+    #     return model
+    # 
+    # vfunc = np.vectorize(lambda arr: 'X' in arr)
+    # fe_sizeT = len(xcolumns(T))  # Nombre de variables de Target
+    # shapeT = (fe_sizeT,)
+    # loss = 'categorical_crossentropy'
+    # clfT = clf_seq(shapeT, nClass=len(np.union1d(np.unique(S.Z), np.unique(T.Z))))
+    # clfT.compile(optimizer='Adam', loss=loss, metrics=['accuracy'])
+    # 
+    # fe_sizeS = len(xcolumns(S))  # Nombre de variables de Target
+    # shapeS = (fe_sizeS,)
+    # loss = 'categorical_crossentropy'
+    # clfS = clf_seq(shapeS, nClass=len(np.union1d(np.unique(S['Z']), np.unique(T['Z']))))
+    # clfS.compile(optimizer='Adam', loss=loss, metrics=['accuracy'])
+    # 
+    # clfT.fit(T.loc[:, xcolumns(T)], enc.fit_transform(zt_estimated.reshape(-1, 1)), batch_size=10,
+    #          epochs=20, verbose=0)  # we train the classifier with target data estimated
+    # clfS.fit(S.loc[:, xcolumns(S)], enc.fit_transform(zs_estimated.reshape(-1, 1)), batch_size=10,
+    #          epochs=20, verbose=0)  # we train the classifier with target data estimated
+    # 
+    # zt_test = clfT.predict(T_test.loc[:, xcolumns(T_test)])
+    # zs_test = clfS.predict(S_test.loc[:, xcolumns(S_test)])
+    # 
+    # zt_test = enc.inverse_transform(zt_test).reshape(-1)
+    # zs_test = enc.inverse_transform(zs_test).reshape(-1)
+    # 
+    # perf_coot_test = (sum(zt_test == T_test.Z) + sum(zs_test == S_test.Z)) / (len(zs_test) + len(zt_test))
+
+    # return perf_coot, perf_coot_test
+    return 0, 0
 
 
 if __name__ == "__main__":
