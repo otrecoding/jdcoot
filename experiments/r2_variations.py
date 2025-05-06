6af0c4a75d9cd7063a0fb5103fa307f40f0a37df
Pierre Navaro <navaro@math.cnrs.fr>
2025-05-06 08:54:59 +0200

    Resolve "r2 variation"

--- experiments/r2_variations.py
+++ experiments/r2_variations.py
@@ -19,7 +19,7 @@ indices = np.random.choice(np.arange(100), math.ceil(0.75 * 100), replace=False)
 nsimulations = 100
 
 train_size = 1000
-test_size = 1000 // 5
+test_size = 1000 // 4
 
 reference_scenario = DataScenario()
 reference_scenario.size_source = train_size
@@ -28,11 +28,11 @@ test_scenario = DataScenarioTest()
 test_scenario.size_source = test_size
 test_scenario.size_target = test_size
     
-json_file = 'observed_labels_proportions_variation.json'
+json_file = 'r2_variations.json'
 with open(json_file, 'w+') as f:
     f.seek(0)
 
-proportions = [0.1, 0.5, 0.9]
+r2_values = [0.2, 0.4, 0.6, 0.8]
 
 for i in range(nsimulations):
     
@@ -42,9 +42,10 @@ for i in range(nsimulations):
         
     source_test = source_test.loc[:, source.columns]
     target_test = target_test.loc[:, target.columns]
+    
+    for r2_source in r2_values:
+        for r2_target in r2_values:
 
-    for prop_source in proportions:
-        for prop_target in proportions:
             for variable_type in variable_types:
                 for learning_method in learning_methods:
                     for recoding_method in recoding_methods:
