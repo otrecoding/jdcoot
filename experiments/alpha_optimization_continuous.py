a1e734f2a0a8f0729748934eb6a7d4909e1c4c2a
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2026-04-10 09:16:49 +0200

    change learning methods in alpha optimization experiment for continuous data

--- experiments/alpha_optimization_continuous.py
+++ experiments/alpha_optimization_continuous.py
@@ -26,7 +26,7 @@ nsimulations = 100
 alpha_values = np.logspace(-6, -3, 10)
 
 variable_types = ["continuous"]
-learning_methods = ["unsupervised"]
+learning_methods = ["partial", "semisupervised"]
 recoding_methods = ["jdcoot"]
 
 sparse_rate = 0.75
