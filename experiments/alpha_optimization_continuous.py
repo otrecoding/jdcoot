eddd6d2387f890269951f337fe83519f69f93dde
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2026-07-22 13:54:09 +0200

    Update alpha_optimization_continuous.py

--- experiments/alpha_optimization_continuous.py
+++ experiments/alpha_optimization_continuous.py
@@ -20,7 +20,7 @@ test.size_target = test_size
 
 json_file = "alpha_optimization_continuous.json"
 with open(json_file, "w+") as f:
-    f.seek(0)
+    f.close()
 
 nsimulations = 100
 alpha_values = np.logspace(-6, -3, 10)
